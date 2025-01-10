721881e21f4a2cac4087c28ebab8a3a148a0eb4b
Alan Matthews <alan.r.matthews@gmail.com>
2025-01-09 19:34:36 -0500

    Day 3

--- AoC2015/day3.py
+++ AoC2015/day3.py
@@ -6,8 +6,39 @@ from Utilities.aoc_day import AdventOfCodeDay
 class Day3(AdventOfCodeDay):
     """Advent of Code Day"""
 
+    def __init__(self, input_file):
+        super().__init__(input_file)
+        with open(input_file, encoding="utf-8") as file:
+            self.data = file.readline().strip()
+
     def part1(self):
-        return None
+        visited = self.visit_houses(self.data, 1)
+        return len(visited)
 
     def part2(self):
-        return None
+        visited = self.visit_houses(self.data, 2)
+        return len(visited)
+
+    def visit_houses(self, data, num_people):
+        visited = set()
+        people = [(0, 0) for _ in range(num_people)]
+        current_person = 0
+
+        visited.add((0, 0))
+
+        for direction in data:
+            x, y = people[current_person]
+            if direction == "^":
+                y += 1
+            elif direction == "v":
+                y -= 1
+            elif direction == ">":
+                x += 1
+            elif direction == "<":
+                x -= 1
+
+            visited.add((x, y))
+            people[current_person] = (x, y)
+            current_person = (current_person + 1) % num_people
+
+        return visited
